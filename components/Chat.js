9161437a8afa35b1bebec89f3edb185c9520b8fa
Sigword <ethan.soto@yahoo.com>
2022-08-18 03:15:31 -0500

    chat

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,27 @@
 import styles from "../styles/chat.module.css";
+import { BsChatDots } from "react-icons/bs";
 
 const Chat = () => {
   return (
     <div className={styles.chatMain}>
       <div className={styles.chatBox}>
         <div className={styles.chatText}>Chat</div>
-        <div className={styles.chatLogo}></div>
+        <div className={styles.chatLogo}>
+          <svg width="0" height="0">
+            <linearGradient
+              id="blue-gradient"
+              x1="100%"
+              y1="100%"
+              x2="0%"
+              y2="0%"
+            >
+              <stop stopColor="#2c0aff" offset="0%" />
+              <stop stopColor="#eb00ff" offset="100%" />
+            </linearGradient>
+          </svg>
+
+          <BsChatDots style={{ fill: "url(#blue-gradient)" }} />
+        </div>
       </div>
     </div>
   );
