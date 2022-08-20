eb4c6d36cd08d9a0de165de1da9a2f76ff9e3938
Umair Ahmed <umairahmed.dev@gmail.com>
2022-08-19 21:26:24 -0500

    calendar component

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,23 +2,33 @@ import styles from "../styles/Calendar.module.css";
 import { useState } from "react";
 
 const Calendar = () => {
-  const [date, setDate] = useState("");
-  const [day, setDay] = useState("");
-  const [currentDateStyles, setCurrentDateStyles] = useState("calendarDate");
-  const [currentDayStyles, setCurrentDayStyles] = useState("calendarDays");
-
-  const days = [1, 2, 3, 4, 5, 6, 7];
-
-  const dateHandler = () => {
+  const calendarCreator = () => {
+    const numberOfDays = [];
     const d = new Date();
-    console.log(d.getDate());
-    console.log(d.getDay());
+
+    const days = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
+
+    for (let i = 0; i < 7; i++) {
+      if (i === d.getDay()) {
+        numberOfDays.push(
+          <div className="calendarDaysSelected" key={i}>
+            <span>{days[i]}</span>
+            <div className="calendarDateSelected">{d.getDate()}</div>
+          </div>
+        );
+      } else {
+        numberOfDays.push(
+          <div className="calendarDays" key={i}>
+            <span>{days[i]}</span>
+            <div className="calendarDate">{d.getDate() - d.getDay() + i}</div>
+          </div>
+        );
+      }
+    }
+
+    return numberOfDays;
   };
 
-  return (
-    <div className={styles.calendarContainer}>
-      <h1>HELL</h1>
-    </div>
-  );
+  return <div className={styles.calendarContainer}>{calendarCreator()}</div>;
 };
 export default Calendar;
